3a43a8043a2c7e031d8d96779549e98d441f8b2f
unknown <s-aelsherif500@zewailcity.edu.eg>
2021-02-24 05:28:02 +0200

    feat: start mongodb project

--- db-connect.js
+++ db-connect.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/todos', {
+mongoose.connect(process.env.MONGO_DB || 'mongodb://localhost:27017/todos', {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
     useFindAndModify:false,
